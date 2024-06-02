c9bcb97537cd80f61a5b06cb25800638be44244a
isriom <62965230+isriom@users.noreply.github.com>
2024-06-01 23:56:04 -0600

    update biblioteca

--- libreria.cpp
+++ libreria.cpp
@@ -7,108 +7,84 @@
 
 using namespace std;
 
-int configured=0;
+int serial_port=-1;
 
-int GetArduinoPort(void)
-{
-    int fd = open("/dev/ttyUSB1", O_RDONLY | O_NOCTTY);
-    if (fd == -1)
+vector<vector<float>> getMax(vector<int> histograma){
+    vector<float> normalizado=vector<float>(histograma.size());
+    vector<float> maxValues=vector<float>();
+    int total=0;
+
+    for (size_t i = 0; i < normalizado.size(); i++){
+        total+=histograma[i];
+    }
+
+    float maxvalue=0;
+    for (size_t i = 0; i < normalizado.size(); i++){
+        normalizado[i]=histograma[i]/total;
+        if (normalizado[i]>normalizado[maxvalue])
+        {
+            maxvalue=i;
+        }
+        
+    }
+
+    maxValues.push_back(maxvalue);
+
+    for (size_t i = 0; i < 4; i++)
     {
-        /* Could not open the port. */
-        perror("open_port: Unable to open /dev/ttyACM0 - ");
+        maxvalue=0;   
+        for (size_t i = 0; i < normalizado.size(); i++){
+            if (normalizado[maxValues.size()-1]>normalizado[i] && normalizado[maxvalue]<normalizado[i])
+            {
+             maxvalue=i;   
+            }
+        }
+        maxValues.push_back(maxvalue);
     }
-
-    char buffer[32];
-    int n = read(fd, buffer, sizeof(buffer));
-    if (n < 0)
-        fputs("read failed!\n", stderr);
-    return (fd);
-}
-int configurePort(int serial_port,int baud_rate = B9600){
-    // Configure the serial port
-    struct termios tty;
-    if (tcgetattr(serial_port, &tty) != 0) {
-        std::cerr << "Error getting terminal attributes: " << strerror(errno) << std::endl;
-        close(serial_port);
-        return 0;
-    }
-
-    tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
-    tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
-    tty.c_cflag &= ~CSIZE;  // Clear all bits that set the data size
-    tty.c_cflag |= CS8;     // 8 bits per byte (most common)
-    tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
-    tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
-
-    tty.c_lflag &= ~ICANON;
-    tty.c_lflag &= ~ECHO; // Disable echo
-    tty.c_lflag &= ~ECHOE; // Disable erasure
-    tty.c_lflag &= ~ECHONL; // Disable new-line echo
-    tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
-    tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
-    tty.c_iflag &= ~(IGNBRK | BRKINT | ICRNL | INLCR | PARMRK | INPCK | ISTRIP); // Disable any special handling of received bytes
-
-    tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
-    tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
-
-    tty.c_cc[VTIME] = 10; // Wait for up to 1s (10 deciseconds), returning as soon as any data is received
-    tty.c_cc[VMIN] = 0;
-
-    cfsetispeed(&tty, baud_rate);
-    cfsetospeed(&tty, baud_rate);
-
-    if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
-        std::cerr << "Error setting terminal attributes: " << strerror(errno) << std::endl;
-        close(serial_port);
-        return 0;
-    }
-    return 1;
+    return {normalizado,maxValues};
 }
 
-
-int main() {
+int draw(vector<int> datos) {
     // Change the port name to match your system
     const char* port_name = "/dev/ttyARDUINO0";
 
     // Open the serial port
-    int serial_port = open(port_name, O_RDWR);
+    if (serial_port<0)
+    {
+        serial_port = open(port_name, O_RDWR);
+    }
+    
 
     if (serial_port < 0) {
         std::cerr << "Error opening serial port: " << strerror(errno) << std::endl;
         return 1;
     }
 
+    std::string message;
+    vector<char> recieved(255);
 
-    if (configured==0)
-    {
-        //configured= configurePort(serial_port);
+    std::cout << "Enter a character to send to the Arduino: ";
+    std::cin >> message;
+
+    // Write the message to the serial port
+    int write_result = write(serial_port, message.c_str(), message.size());
+    if (write_result < 0) {
+        std::cerr << "Error writing to serial port: " << strerror(errno) << std::endl;
+    } else {
+        std::cout << "Sent: " << message << std::endl;
+    }
+    
+    int read_result = read(serial_port,recieved.data(),255);
+    if (read_result < 0) {
+        std::cerr << "Error Reading to serial port: " << strerror(errno) << std::endl;
+    } else {
+        std::cout << "readed: " << recieved.data() << std::endl;
     }
     
 
-
-
-    std::string message;
-    vector<char> recieved(255);
-    while (true) {
-        std::cout << "Enter a character to send to the Arduino: ";
-        std::cin >> message;
-
-        // Write the message to the serial port
-        int write_result = write(serial_port, message.c_str(), message.size());
-        if (write_result < 0) {
-            std::cerr << "Error writing to serial port: " << strerror(errno) << std::endl;
-        } else {
-            std::cout << "Sent: " << message << std::endl;
-        }
-        
-        int read_result = read(serial_port,recieved.data(),255);
-        if (read_result < 0) {
-            std::cerr << "Error Reading to serial port: " << strerror(errno) << std::endl;
-        } else {
-            std::cout << "readed: " << recieved.data() << std::endl;
-        }
-    }
-
     close(serial_port);
     return 0;
+}
+int main(){
+    draw(vector<int>(256));
 }
